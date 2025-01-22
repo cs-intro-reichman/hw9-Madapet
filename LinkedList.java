67e60505f41bfff69bde3c4c1a73bc66efd89e32
adam <mada.carpm>
2025-01-22 13:58:47 +0200

    new

--- LinkedList.java
+++ LinkedList.java
@@ -3,6 +3,36 @@
  */
 public class LinkedList {
 
+	public static void main(String[] args) {
+		MemoryBlock block1 = new MemoryBlock(1, 1);
+		MemoryBlock block2 = new MemoryBlock(1, 2);
+		MemoryBlock block3 = new MemoryBlock(1, 3);
+		MemoryBlock block4 = new MemoryBlock(1, 4);
+		Node node1 = new Node(block1);
+		Node node2 = new Node(block2);
+		Node node3 = new Node(block3);
+		Node node4 = new Node(block4);
+
+		// LinkedList trylist = new LinkedList();
+		// System.out.println(trylist.toString());
+
+		// //Try add
+		// trylist.add(0, block1);
+		// System.out.println(trylist.toString());
+
+		// //Try addFirst
+
+		// trylist.addFirst(block2);
+		// System.out.println(trylist.toString());
+
+		// //Try addLast
+
+		// trylist.addLast(block1);
+		// System.out.println(trylist.toString());
+
+
+	}
+
 	private Node first; // pointer to the first element of this list
 	private Node last; // pointer to the last element of this list
 	private int size; // number of elements in this list
@@ -18,7 +48,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the first node of the list
-	 * 
+	 *
 	 * @return The first node of the list.
 	 */
 	public Node getFirst() {
@@ -27,7 +57,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the last node of the list
-	 * 
+	 *
 	 * @return The last node of the list.
 	 */
 	public Node getLast() {
@@ -36,7 +66,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the current size of the list
-	 * 
+	 *
 	 * @return The size of the list.
 	 */
 	public int getSize() {
@@ -45,7 +75,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the node located at the given index in this list.
-	 * 
+	 *
 	 * @param index
 	 *              the index of the node to retrieve, between 0 and size
 	 * @throws IllegalArgumentException
@@ -78,7 +108,7 @@ public class LinkedList {
 	 * <p>
 	 * The method implementation is optimized, as follows: if the given
 	 * index is either 0 or the list's size, the addition time is O(1).
-	 * 
+	 *
 	 * @param block
 	 *              the memory block to be inserted into the list
 	 * @param index
@@ -89,11 +119,14 @@ public class LinkedList {
 	 */
 	public void add(int index, MemoryBlock block) {
 		Node newNode = new Node(block);
+		// System.out.println(index);
 		if (index == 0) {
 			newNode.next = this.first;
 			this.first = newNode;
+			if (size ==0){this.last = this.first;}
 		} else {
 			if (index == size) {
+				//System.out.println("last is " + this.last);
 				this.last.next = newNode;
 				this.last = newNode;
 			} else {
@@ -109,7 +142,7 @@ public class LinkedList {
 	/**
 	 * Creates a new node that points to the given memory block, and adds it
 	 * to the end of this list (the node will become the list's last element).
-	 * 
+	 *
 	 * @param block
 	 *              the given memory block
 	 */
@@ -122,7 +155,7 @@ public class LinkedList {
 	 * Creates a new node that points to the given memory block, and adds it
 	 * to the beginning of this list (the node will become the list's first
 	 * element).
-	 * 
+	 *
 	 * @param block
 	 *              the given memory block
 	 */
@@ -133,7 +166,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the memory block located at the given index in this list.
-	 * 
+	 *
 	 * @param index
 	 *              the index of the retrieved memory block
 	 * @return the memory block at the given index
@@ -149,7 +182,7 @@ public class LinkedList {
 
 	/**
 	 * Gets the index of the node pointing to the given memory block.
-	 * 
+	 *
 	 * @param block
 	 *              the given memory block
 	 * @return the index of the block, or -1 if the block is not in this list
@@ -166,7 +199,7 @@ public class LinkedList {
 
 	/**
 	 * Removes the given node from this list.
-	 * 
+	 *
 	 * @param node
 	 *             the node that will be removed from this list
 	 */
@@ -178,7 +211,7 @@ public class LinkedList {
 
 	/**
 	 * Removes from this list the node which is located at the given index.
-	 * 
+	 *
 	 * @param index the location of the node that has to be removed.
 	 * @throws IllegalArgumentException
 	 *                                  if index is negative or greater than or
@@ -191,7 +224,7 @@ public class LinkedList {
 
 	/**
 	 * Removes from this list the node pointing to the given memory block.
-	 * 
+	 *
 	 * @param block the memory block that should be removed from the list
 	 * @throws IllegalArgumentException
 	 *                                  if the given memory block is not in this
@@ -214,9 +247,9 @@ public class LinkedList {
 	 */
 	public String toString() {
 		//// Replace the following statement with your code
-		String linkedList = "LinkedList";
+		String linkedList = "";
 		for (int i = 0; i < size; i++) {
-			linkedList = linkedList + " -> " + getNode(i).toString();
+			linkedList = linkedList  + getNode(i).toString();
 		}
 		return linkedList;
 	}
